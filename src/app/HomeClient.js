2fc13ec0d08c91b8a9a8954c9dc3d225169124d7
baliheadtour <headtourbali@gmail.com>
2026-09-01 16:35:09 +0800

    style: fix sub-pixel rendering gaps on campaign cutout

--- src/app/HomeClient.js
+++ src/app/HomeClient.js
@@ -964,14 +964,14 @@ export default function HomeClient({ initialListings = [], initialSettings = nul
 
                 {/* Top Right Click Button with Curve Cutout Effect */}
                 {!camp.isHeroSlide && (
-                  <div className="absolute top-0 right-0 z-20 pointer-events-auto">
-                    <div className="bg-[#D4F84E] rounded-bl-[24px] pl-3 pb-3 relative flex items-center gap-2">
+                  <div className="absolute -top-[1px] -right-[1px] z-20 pointer-events-auto">
+                    <div className="bg-[#D4F84E] rounded-bl-[24px] pl-3 pb-3 pr-[1px] pt-[1px] relative flex items-center gap-2">
                       {/* Left Curve SVG */}
-                      <svg className="absolute top-0 -left-[24px] w-[24px] h-[24px] fill-[#D4F84E]" viewBox="0 0 24 24">
+                      <svg className="absolute top-[1px] -left-[23.5px] w-[24px] h-[24px] fill-[#D4F84E]" viewBox="0 0 24 24">
                         <path d="M24 0H0C13.2548 0 24 10.7452 24 24V0Z" />
                       </svg>
                       {/* Bottom Curve SVG */}
-                      <svg className="absolute -bottom-[24px] right-0 w-[24px] h-[24px] fill-[#D4F84E]" viewBox="0 0 24 24">
+                      <svg className="absolute -bottom-[23.5px] right-[1px] w-[24px] h-[24px] fill-[#D4F84E]" viewBox="0 0 24 24">
                         <path d="M24 0H0C13.2548 0 24 10.7452 24 24V0Z" />
                       </svg>
 
